7288614eb104c7cd84afd2a41d93385636d61611
LeonidHome <defenser2205@gmail.com>
2023-02-07 20:27:51 +0300

    task 1-2

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
+
+    public static void checkLeapYear(int year) {
+        if (year % 4 == 0 || year % 400 == 0 && year % 100 != 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int year = 2023;
+        checkLeapYear(year);
+    }
+
+    public static void checkOSAndYear(byte clientOS, int clientDeviceYear) {
+        if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        byte clientOS = 1;
+        int clientDeviceYear = 2021;
+        checkOSAndYear(clientOS, clientDeviceYear);
+    }
+}
